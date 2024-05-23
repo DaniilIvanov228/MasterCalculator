7e3d84201009189b629fd250f357636dc97f9378
USER WERTUS <daniilivanov833@gmail.com>
2024-05-23 22:49:51 +0900

    Добавьте файлы проекта.

---
+++ Item.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator
+{
+    //Класс структуры элемента формулы(имя и значение) в коллекции
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string Description { get; set; }
+    }
+}
